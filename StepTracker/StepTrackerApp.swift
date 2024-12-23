af404f8bad52bd86a1c9d6f82cefc62989bb8d5f
alcode111 <al.code.111@gmail.com>
2024-12-23 07:42:24 +0100

    Import Xcode Project

---
+++ StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Ismaïl on 23/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
